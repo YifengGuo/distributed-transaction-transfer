7be1ae6e899d0b343c4b10063e2046df1bd0a682
YifengGuo <gyf940517@gmail.com>
2020-11-02 10:47:10 +0800

    impl server registry server and update log config

--- service-registry-server/src/main/java/com/yifeng/RegistryApp.java
+++ service-registry-server/src/main/java/com/yifeng/RegistryApp.java
@@ -3,11 +3,12 @@ package com.yifeng;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author guoyifeng on 11/1/20
  */
-@EnableEurekServer
+@EnableEurekaServer
 @SpringBootApplication
 public class RegistryApp {
     public static void main(String[] args) {
